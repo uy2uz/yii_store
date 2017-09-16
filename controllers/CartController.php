1cb8badcb2962522d10c1d0496c06a2a0d863f56
AlexKuriakov <akuryakov@gmail.com>
2017-09-17 01:41:45 +0300

    v.a.0.0.3_17_09_2017_add_save_order

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -3,6 +3,9 @@
 namespace app\controllers;
 use app\models\Product;
 use app\models\Cart;
+use app\models\Order;
+use app\models\OrderItems;
+
 use Yii;
 
 class CartController extends AppController {
@@ -51,10 +54,39 @@ class CartController extends AppController {
         $this->layout = false;
         return $this->render('cart-modal', compact('session'));
     }
-
     public function actionView(){
-        return $this->render('view');
+        $session = Yii::$app->session;
+        $session->open();
+        $this->setMeta('Корзина');
+        $order = new Order();
+        if( $order->load(Yii::$app->request->post()) ){
+            $order->qty = $session['cart.qty'];
+            $order->sum = $session['cart.sum'];
+            if($order->save()){
+                $this->saveOrderItems($session['cart'], $order->id);
+                Yii::$app->session->setFlash('success', 'Ваш заказ принят. Менеджер вскоре свяжется с Вами.');
+                
+                $session->remove('cart');
+                $session->remove('cart.qty');
+                $session->remove('cart.sum');
+                
+                return $this->refresh();
+            }else{
+                Yii::$app->session->setFlash('error', 'Ошибка оформления заказа');
+            }
+        }
+        return $this->render('view', compact('session', 'order'));
+    } 
+    protected function saveOrderItems($items, $order_id){
+        foreach($items as $id =>$item){
+            $order_items = new OrderItems();
+            $order_items->order_id = $order_id;
+            $order_items->product_id = $id;
+            $order_items->name = $item['name'];
+            $order_items->price =$item['price'];
+            $order_items->qty_item = $item['qty'];
+            $order_items->sum_item = $item['qty'] * $item['price'];
+            $order_items->save();
+        }
     }
-    
-    
 }
--- views/cart/view.php
+++ views/cart/view.php
@@ -1,1 +1,70 @@
-Коззина
+<?php
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\widgets\ActiveForm;
+
+?>
+<div class = "container">
+    <?php if(Yii::$app->session->hasFlash('success')):?>
+        <div class="alert alert-success alert-dismissable" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <?php echo Yii::$app->session->getFlash('success');?>
+        </div>
+    <?php endif;?>
+    
+    <?php if(Yii::$app->session->hasFlash('error')):?>
+        <div class="alert alert-danger alert-dismissable" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <?php echo Yii::$app->session->getFlash('error');?>
+        </div>
+    <?php endif;?>
+    
+    <?php if(!empty($session['cart'])): ?>
+    <hr>
+    <div class="table-responsive">
+        <table class = "table table-hover table-striped">
+            <thead>
+                <tr>
+                    <th>Фото</th>
+                    <th>Наименование</th>
+                    <th>Количество</th>
+                    <th>Цена товара</th>
+                    <th>Стоимость</th>
+                    <th>Действие</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($session['cart'] as $id => $item):?>
+                    <tr>
+                        <td><?php echo \yii\helpers\Html::img("@web/images/products/{$item['img']}", ['alt' => $item['name'], 'height' => 50 ])?></td>
+                        <td><a href="<?php echo Url::to(['product/view', 'id' => $id])?>"><?php echo $item['name']?></а></td>
+                        <td><?php echo $item['qty']?></td>
+                        <td><?php echo $item['price']?></td>
+                        <td><?php echo ($item['qty'] * $item['price'])?></td>
+                        <td><span data-id="<?php echo $id?>" class="glyphicon glyphicon-remove text-danger del-item" aria-hidden="true"></span></td>
+                    </tr>
+                <?php endforeach;?>
+                    <tr>
+                        <td colspan="5">Количество товара: </td>
+                        <td><?php echo $session['cart.qty']?></td>
+                    </tr>
+                    <tr>
+                        <td colspan="5">Сумма к оплате: </td>
+                        <td><?php echo $session['cart.sum']?></td>
+                    </tr>
+            </tbody>
+        </table>
+    </div>
+    </hr>
+    <?php $form = ActiveForm::begin()?>
+    
+    <?php echo $form->field($order, 'name')?>
+    <?php echo $form->field($order, 'email')?>
+    <?php echo $form->field($order, 'phone')?>
+    <?php echo $form->field($order, 'address')?>
+    <?php echo Html::submitButton('Заказать', ['class' => 'btn btn-success'])?>
+    <?php ActiveForm::end()?>
+    <?php else: ?>
+        <h3>Корзина пуста</h3>
+    <?php endif; ?>
+</div>
